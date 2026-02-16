3211447d99be870c4c50190519ad6be0acafe83c
GEOVANNY PC <geovannygrefa86@gmail.com>
2026-02-16 16:11:45 -0500

    Proyecto1

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -3,6 +3,20 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return 'Hello, World! clase de programación  Tutoría Semana 9'
+    return 'Hello, World! Tarea de Desarrollo de Aplicaciones Web Semana 9'
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+@app.route('/producto/<nombre>')
+def producto(nombre):
+    return f"Producto: {nombre} – disponible."
+
+@app.route('/contactos')
+def contactos():
+    return 'Puedes contactar a traves de nuestro correo electronico: geovannygrefa86@gmail.com'
+
 if __name__ == '__main__':
     app.run(debug=True)
+
